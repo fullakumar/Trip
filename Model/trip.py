53c701b94a048bc4b40e842e47a8673dc8f85db2
fullakumar <128395206+fullakumar@users.noreply.github.com>
2025-07-27 00:09:40 +0530

    Update trip.py

--- Model/trip.py
+++ Model/trip.py
@@ -7,8 +7,8 @@ from zoneinfo import ZoneInfo
 def get_current_time_str():
     return datetime.now(ZoneInfo("Asia/Kolkata")).strftime("%I:%M %p")
 
-def get_utc_date():
-    return datetime.now(timezone.utc).date()
+def get_local_date():
+    return datetime.now(ZoneInfo("Asia/Kolkata")).date()
 
 class Trip(Base):
     __tablename__ = "trip_data"
@@ -17,6 +17,6 @@ class Trip(Base):
     name = Column(String, nullable=False)
     price = Column(String, nullable=False)
     paid_by = Column(String, nullable=False)
-    date = Column(Date, default=get_utc_date)
+    date = Column(Date, default=get_local_date)
     time = Column(String, default=get_current_time_str)
 
